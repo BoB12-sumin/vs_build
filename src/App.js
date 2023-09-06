e1ebfbadad30ece95bde884de45438aa07e942e2
hellokms8828 <hellokms8828@naver.com>
2023-09-06 16:12:30 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,116 +1,107 @@
-import React from "react";
-import * as go from "gojs";
-import { ReactDiagram } from "gojs-react";
-
 import "./App.css";
-//import Pallete from "./components/Palette";
+import React, { useEffect } from "react";
+import * as go from "gojs";
+import { ToolManager, Diagram } from "gojs";
+import { ReactDiagram } from "gojs-react";
+import Pallete from "./components/Palette";
 
 function initDiagram() {
   const $ = go.GraphObject.make;
-  // set your license key here before creating the diagram: go.Diagram.licenseKey = "...";
   const diagram = $(go.Diagram, {
     "undoManager.isEnabled": true,
-    //layout: new go.TreeLayout({ angle: 90, layerSpacing: 35 }),
+    "resizingTool.isEnabled": true,
+    model: new go.GraphLinksModel({ linkKeyProperty: "key" }),
+    "linkingTool.isEnabled": true, // enable linking tool
     "clickCreatingTool.archetypeNodeData": {
-      key: 4,
-      text: "CAT",
+      text: "new node",
       color: "lightblue",
-      source: "img/cat.png",
     },
-    model: new go.GraphLinksModel({
-      linkKeyProperty: "key", // IMPORTANT! must be defined for merges and data sync when using GraphLinksModel
-    }),
   });
 
-  // define a simple Node template
   diagram.nodeTemplate = $(
     go.Node,
     "Auto",
-    { background: "#44CCFF" },
-    new go.Binding("location", "loc", go.Spot.Center).makeTwoWay(
+    { resizable: true, resizeObjectName: "SHAPE" },
+    new go.Binding("location", "loc", go.Point.parse).makeTwoWay(
       go.Point.stringify
     ),
     $(
-      go.Picture,
-      {
-        margin: 10,
-        width: 50,
-        height: 50,
-        background: "lightblue",
-        portId: "",
-        cursor: "pointer",
-        fromLinkable: true,
-        fromLinkableSelfNode: true,
-        fromLinkableDuplicates: true,
-        toLinkable: true,
-        toLinkableSelfNode: true,
-        toLinkableDuplicates: true,
-      },
-      new go.Binding("source").makeTwoWay()
+      go.Shape,
+      { name: "SHAPE", fill: "white", strokeWidth: 0 },
+      new go.Binding("fill", "color"),
+      new go.Binding("figure")
     ),
-    $(
-      go.TextBlock,
-      "Default Text",
-      { stroke: "white", font: "bold 12px sans-serif" },
-      new go.Binding("text").makeTwoWay()
-    )
-  );
+    $(go.TextBlock, { margin: 8, editable: true }),
+    //new go.Binding('text').makeTwoWay()),
 
-  diagram.linkTemplate = $(
-    go.Link,
+    // make the Shape deletable by right-clicking or pressing the Delete key
     {
-      routing: go.Link.Orthogonal,
-      corner: 5,
-      relinkableFrom: true,
-      relinkableTo: true,
+      contextMenu: $(
+        "ContextMenu",
+        $("ContextMenuButton", $(go.TextBlock, "Delete"), {
+          click: function (e, obj) {
+            e.diagram.commandHandler.deleteSelection();
+          },
+        })
+      ),
     },
-    $(go.Shape, { strokeWidth: 3, stroke: "#555" })
-    //$(go.Shape, { toArrow: "Standard", stroke: null })
+    makePort("T", go.Spot.Top), //각각 다이어그램의 상하좌우에 포트를 생성한다
+    makePort("L", go.Spot.Left),
+    makePort("R", go.Spot.Right),
+    makePort("B", go.Spot.Bottom)
   );
+  // enable Ctrl+Z to undo and Ctrl+Y to redo
 
-  diagram.addDiagramListener("ObjectSingleClicked", function (e) {
-    const node = e.subject.part;
-    if (node instanceof go.Node) {
-      console.log(node.data);
-    }
-  });
+  function makePort(name, spot) {
+    return $(go.Shape, {
+      fill: "transparent",
+      stroke: null,
+      desiredSize: new go.Size(15, 15), //여기가 노드를 선택하는 포트의 크기를 조정하는 곳
+      alignmentFocus: spot,
+      alignment: spot.opposite(),
+      portId: name,
+      fromLinkable: true,
+      toLinkable: true,
+    });
+  }
 
   return diagram;
 }
 
+/*function handleModelChange(changes) {
+  alert('GoJS model changed!');
+}
+*/
+
 function App() {
+  const nodeDataArrayPalette = [
+    { key: -1, text: "Start", figure: "Circle", color: "lightblue" },
+    { key: -2, text: "Step", figure: "Rectangle", color: "lightblue" },
+    { key: -3, text: "???", figure: "Diamond", color: "lightblue" },
+    { key: -4, text: "End", figure: "Circle", color: "lightblue" },
+  ];
   return (
-    <div className="App">
+    <div className="App" style={{ display: "flex", flexDirection: "row" }}>
       <ReactDiagram
         initDiagram={initDiagram}
         divClassName="diagram-component"
         nodeDataArray={[
-          {
-            key: 0,
-            loc: "0 0",
-            text: "1",
-            source: "img/cat.png",
-          },
-          {
-            key: 1,
-            loc: "0 100",
-            text: "2",
-            source: "img/icon2.png",
-          },
-          {
-            key: 2,
-            loc: "150 0",
-            text: "3",
-            source: "img/icon1.png",
-          },
+          { key: 0, text: "Alpha", color: "lightblue", loc: "0 0" },
+          { key: 1, text: "Beta", color: "orange", loc: "150 0" },
+          { key: 2, text: "Gamma", color: "lightgreen", loc: "0 150" },
+          { key: 3, text: "Delta", color: "pink", loc: "150 150" },
         ]}
         linkDataArray={[
           { key: -1, from: 0, to: 1 },
           { key: -2, from: 0, to: 2 },
-          { key: -3, from: 2, to: 2 },
+          //{ key: -3, from: 1, to: 1 },
+          { key: -4, from: 2, to: 3 },
+          { key: -5, from: 3, to: 0 },
         ]}
         //onModelChange={handleModelChange}
       />
+
+      <Pallete nodeDataArray={nodeDataArrayPalette} />
     </div>
   );
 }
